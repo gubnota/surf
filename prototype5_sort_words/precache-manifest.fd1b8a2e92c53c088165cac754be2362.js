757baa9cce0e1fe5eb725d861873ff7f32017e14
Larry Moore <hvw@yandex.com>
2021-04-21 09:28:45 +0800

    Add files via upload

---
+++ prototype5_sort_words/precache-manifest.fd1b8a2e92c53c088165cac754be2362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bbcb1467f60ed6181ee3c58188de3d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c691180f34f71b3c9b3c",
+    "url": "/static/css/main.2cb51f90.chunk.css"
+  },
+  {
+    "revision": "5ee7132e81e0eb90b113",
+    "url": "/static/js/2.78ec5a64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.78ec5a64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c691180f34f71b3c9b3c",
+    "url": "/static/js/main.13e29425.chunk.js"
+  },
+  {
+    "revision": "864a1a4968323d26acfb",
+    "url": "/static/js/runtime-main.5c2b7b9f.js"
+  }
+]);
